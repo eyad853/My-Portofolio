a94bffd0845e342e05a6203e2d1cf03d031c43aa
eyad <eyadmosa853@gmail.com>
2025-01-28 01:40:14 +0300

    Reset GitHub Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio/', // Replace 'Portfolio' with your repository name
-
   plugins: [react()],
 })
-
