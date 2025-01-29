67050f526bcefaeef9356f0557157484cd16837e
eyad <eyadmosa853@gmail.com>
2025-01-30 02:29:10 +0300

    finally

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/My-Portofolio/', // Ensure this matches your GitHub repo name exactly
+  build: {
+    outDir: 'dist',
+  },
 });
