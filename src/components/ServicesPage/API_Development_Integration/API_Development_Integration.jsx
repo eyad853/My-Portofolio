a5cb70206154174350f067902f03b09059fb7579
eyad <eyadmosa853@gmail.com>
2025-02-01 00:09:51 +0300

    Reupload portfolio files

--- src/components/ServicesPage/API_Development_Integration/API_Development_Integration.jsx
+++ src/components/ServicesPage/API_Development_Integration/API_Development_Integration.jsx
@@ -5,7 +5,7 @@ import Nav from '../../Nav/Nav'
 const API_Development_Integration = () => {
   return (
   <div className='text-white overflow-hidden'>
-    <div className='w-screen h-full bg-[url("_bd71bd68-32fa-438f-a1c1-20e6bfbdfc64.jpeg")] bg-cover bg-bottom bg-no-repeat '>
+    <div className='w-screen h-full bg-[url("/_bd71bd68-32fa-438f-a1c1-20e6bfbdfc64.jpeg")] bg-cover bg-bottom bg-no-repeat '>
         <Nav />
         <APIBody />
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ import ThirdSection from '../../components/ThirdSection/ThirdSection'
 const Home = () => {
   return (
     <div className='text-white overflow-hidden'>
-        <div className='w-screen h-full bg-[url("_bd71bd68-32fa-438f-a1c1-20e6bfbdfc64.jpeg")] bg-cover bg-bottom bg-no-repeat '>
+        <div className='w-screen h-full bg-[url("/_bd71bd68-32fa-438f-a1c1-20e6bfbdfc64.jpeg")] bg-cover bg-bottom bg-no-repeat '>
             <Nav />
             <FirstSection />
             <SecondSection />
