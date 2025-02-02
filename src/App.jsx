a2e9847007ec7268d273f404a344e476f29d2668
eyad <eyadmosa853@gmail.com>
2025-02-02 15:45:28 +0300

    fix what u did

--- src/App.jsx
+++ src/App.jsx
@@ -55,13 +55,13 @@ const App = () => {
         <Route path='/skills/Typescript' element={<TypeScriptPage   />} />
         <Route path='/skills/HTML' element={<HtmlPage   />} />
         <Route path='/projects/firstProject' element={<ProjectPage   
-        firstImage="./notessignup.png"
-        secondImage="./noteslogin.png"
-        thirdImage="./emptynotes.png"
-        fourthImage="./notes.png"
-        firstVideo="./signupnotes.mp4"
-        secondVideo="./loginnotes.mp4"
-        thirdVideo="./notes.mp4"
+        firstImage="/notessignup.png"
+        secondImage="/noteslogin.png"
+        thirdImage="/emptynotes.png"
+        fourthImage="/notes.png"
+        firstVideo="/signupnotes.mp4"
+        secondVideo="/loginnotes.mp4"
+        thirdVideo="/notes.mp4"
         note={true}
         noteText="This project is based entirely on a YouTube tutorial, where I followed along to create a fully functional notes app. Through this tutorial, I learned and practiced:
         Password Encryption: Using bcrypt to hash and verify passwords for secure user authentication.
@@ -80,13 +80,13 @@ const App = () => {
         Delete Notes: Finally, the video demonstrates the ability to delete notes seamlessly, with visual feedback like confirmation prompts or instant removal from the list."
         />} />
         <Route path='/projects/secondProject' element={<ProjectPage   
-        firstImage="./todomyday.png"
-        secondImage="./todoimportant.png"
-        thirdImage="./todoplanned.png"
-        fourthImage="./todotasks.png"
-        firstVideo="./todomyimportantv.mp4"
-        secondVideo="./todoplannedv.mp4"
-        thirdVideo="./todotasksv.mp4"
+        firstImage="/todomyday.png"
+        secondImage="/todoimportant.png"
+        thirdImage="/todoplanned.png"
+        fourthImage="/todotasks.png"
+        firstVideo="/todomyimportantv.mp4"
+        secondVideo="/todoplannedv.mp4"
+        thirdVideo="/todotasksv.mp4"
         firstImageText="The My Day page is designed to help users focus on tasks that matter today. It displays tasks that are scheduled for the current day or those manually added by the user for immediate attention. With an intuitive layout, users can easily plan and prioritize their daily responsibilities."
         secondImageText="The Important page keeps track of tasks marked as high-priority. By flagging tasks, users can quickly filter and view items that need their immediate focus. This page ensures that no critical tasks are overlooked."
         thirdImageText="The Planned page organizes tasks based on scheduled dates. It provides a clear overview of tasks planned for the upcoming days, weeks, or months. This page is perfect for long-term planning and staying ahead of deadlines."
