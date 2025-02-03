67d07e102e2d8d4eb872776bd804933514b62abb
eyad <eyadmosa853@gmail.com>
2025-02-03 04:44:27 +0300

    back2

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ const App = () => {
         <Route path='/skills/Typescript' element={<TypeScriptPage   />} />
         <Route path='/skills/HTML' element={<HtmlPage   />} />
         <Route path='/projects/firstProject' element={<ProjectPage   
-        firstImage={`${process.env.PUBLIC_URL}/images/notessignup.png`}
+        firstImage="/notessignup.png"
         secondImage="/noteslogin.png"
         thirdImage="/emptynotes.png"
         fourthImage="/notes.png"
