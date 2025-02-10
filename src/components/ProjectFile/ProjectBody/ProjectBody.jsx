d2d486e3dee58f82c7d0b6e7587f9ed213309c6d
eyad <eyadmosa853@gmail.com>
2025-02-10 14:27:21 +0300

    try to change

--- src/components/ProjectFile/ProjectBody/ProjectBody.jsx
+++ src/components/ProjectFile/ProjectBody/ProjectBody.jsx
@@ -129,7 +129,7 @@ return (
                 {/* videos */}
                 <div className='relative bg-gradient-to-tl from-slate-600 to-slate-900 w-72 h-60 hover:w-80 hover:h-64 transition-all duration-200 rounded-2xl mt-20 '>
                     <video 
-                    src={`${import.meta.env.BASE_URL}${firstVideo}`} 
+                    src={`https://my-portofolio-nu-silk.vercel.app/My-Portofolio/projects/firstProject${firstVideo}`} 
                     controls 
                     autoPlay 
                     loop  
