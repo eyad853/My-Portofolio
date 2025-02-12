b883a2f56c7a3ab97459d6d7ecceff6c059cc747
eyad <eyadmosa853@gmail.com>
2025-02-12 19:13:36 +0300

    back to safe

--- src/App.jsx
+++ src/App.jsx
@@ -59,9 +59,9 @@ const App = () => {
         secondImage="noteslogin.png"
         thirdImage="emptynotes.png"
         fourthImage="notes.png"
-        firstVideo="/videos/notesSignUp_fixed.mp4"
-        secondVideo="/videos/notesLogin_fixed.mp4"
-        thirdVideo="/videos/notes_fixed.mp4"
+        firstVideo="/My-Portofolio/videos/notesSignUp_fixed.mp4"
+        secondVideo="/My-Portofolio/videos/notesLogin_fixed.mp4"
+        thirdVideo="/My-Portofolio/videos/notes_fixed.mp4"
         note={true}
         noteText="This project is based entirely on a YouTube tutorial, where I followed along to create a fully functional notes app. Through this tutorial, I learned and practiced:
         Password Encryption: Using bcrypt to hash and verify passwords for secure user authentication.
