8af9661b86e1df23b31b6739089fa20a12aa1485
eyad <eyadmosa853@gmail.com>
2025-02-14 12:39:16 +0300

    somethingIforgie

--- src/components/ProjectsPages/ProjectsPages.jsx
+++ src/components/ProjectsPages/ProjectsPages.jsx
@@ -8,13 +8,13 @@ const ProjectsPages = () => {
             {/* first project */}
             <div className=' relative bg-slate-500 w-96 h-96 ml-56  mt-14 rounded-2xl bg-gradient-to-tl from-slate-600 to-slate-900 transform hover:scale-105 transition-all duration-200'>
               <img src="/My-Portofolio/notes.webp" className='w-full h-full rounded-2xl absolute' alt="" />
-              <Link to={"/projects/firstProject"} className='absolute w-full h-full z-50'></Link>
+              <Link to={"/firstProject"} className='absolute w-full h-full z-50'></Link>
             </div>  
 
             {/* second projcet */}
             <div className='relative bg-slate-500 w-96 h-96 mt-14 rounded-2xl bg-gradient-to-tl from-slate-600 to-slate-900 transform hover:scale-105 transition-all duration-200'>
             <img src="/My-Portofolio/todo.webp" className='w-full h-full rounded-2xl absolute' alt="" />
-            <Link to={"/projects/secondProject"} className='absolute w-full h-full z-50'></Link>
+            <Link to={"/secondProject"} className='absolute w-full h-full z-50'></Link>
             </div>
 
           
